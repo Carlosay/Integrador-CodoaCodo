7a13d0e6029d6e5929c5ece892a5631a6a3e3855
Carlosay <carlosay_20@live.com.ar>
2022-05-24 23:24:13 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -13,3 +13,38 @@ const changeLinkState = () => {
 changeLinkState();
 
 window.addEventListener('scroll', changeLinkState);
+
+
+/***** Calculo de presion al presionar Resumen *****/
+const form_ticket = document.getElementById('form_ticket')
+
+const calcularPrecio = () => {
+  const cantidad = document.getElementById('cantidad').value
+  const categoria = document.getElementById('categoria').value
+  let precio = 200 * cantidad  
+
+  if (categoria == 'E') {
+    precio -= precio * 0.8
+  } else if (categoria == 'T') {
+    precio -= precio * 0.5
+  } else if (categoria == 'J') {
+    precio -= precio * 0.15
+  }
+
+  document.getElementById('total').innerHTML = precio
+}
+
+form_ticket.addEventListener('submit', e => {
+  e.preventDefault()
+  calcularPrecio()
+})
+
+form_ticket.addEventListener('reset', e => { 
+  document.getElementById('total').innerHTML = '' 
+})
+
+document.getElementById('cantidad').addEventListener('change', e => {
+  if(e.target.value < 1) {
+    e.target.value = 1
+  }
+})
